c16cc12fb810ab854925d9378dbdf5c7ccf911b6
Sergei_L <lubeshkosergei@gmail.com>
2021-02-15 07:52:16 +0300

    2 lessons

--- api/hotel/views.py
+++ api/hotel/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from backend.hotel.models import HotelRooms, BookingRoom
+from api.hotel.serializer import (HotelRoomsSerializers, BookingRoomListSerializer, BookingRoomSerializer)
+
+
+class HotelRoomsList(generics.ListAPIView):  # Any user can see the numbers
+    '''Список номеров отеля -List room hotel'''
+    permission_classes = [permissions.AllowAny]
+    queryset = HotelRooms.objects.all()
+    serializer_class = HotelRoomsSerializers
+
+
+class BookingRoomList(generics.ListAPIView):
+    """Список забронированных номеров"""
+    permission_classes = [permissions.IsAdminUser]
+    queryset = BookingRoom.objects.all()
+    serializer_class = BookingRoomListSerializer
+
+
+class BookingRoomRecord(generics.CreateAPIView):
+    """Запись брони в БД"""
+    permission_classes = [permissions.AllowAny]
+    queryset = BookingRoom.objects.all()
+    serializer_class = BookingRoomSerializer #BookinRoomSerializer
